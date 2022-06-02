f7d84e9070e30c563e5ebfbbf5e638fed88a50d0
Juruoer <1461615022@qq.com>
2022-06-02 23:14:14 +0800

    Site updated: 2022-06-02 23:14:11

---
+++ js/custom/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "012",
+        "background": "5",
+        "tmpColor": "4A86E8",
+        "tmpSize": "15",
+        "cityColor": "FF9900",
+        "citySize": "15",
+        "aqiColor": "4A86E8",
+        "aqiSize": "15",
+        "weatherIconSize": "22",
+        "alertIconSize": "16",
+        "padding": "3px 0px 0px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "false",
+        "vertical": "center",
+        "horizontal": "left",
+        "left": "0",
+        "top": "0",
+        "key": "d4ff0bdd476c4e72b12ee593fd98caa3"
+    }
+}
