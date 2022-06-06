64d5ebfefa680ad6350fffe73605d77124684106
Juruoer <1461615022@qq.com>
2022-06-06 23:14:52 +0800

    Site updated: 2022-06-06 23:14:47

--- js/custom/welcome.js
+++ js/custom/welcome.js
@@ -1,7 +1,7 @@
 function welcome() { // 欢迎弹窗
     Swal.fire({
         title: ' 欢迎！',
-        html: "<div class = 'welcome-swal-html'><i class='iconfont icon-lihua'></i> 欢迎来到 Juruoer 的博客！</br> <i class='iconfont icon-yinle1'></i> 左下角可以打开音乐播放器，不知道有没有合你胃口的音乐呢？</br > <i class='iconfont icon-shezhi1'></i> 右下角点击 <i class= 'fas fa-cog fa-spin'></i > 可以展开更多设置。</div>",
+        html: "<div class = 'welcome-swal-html'><i class='iconfont icon-lihua'></i> 欢迎来到 Juruoer 的博客！</br> <i class='iconfont icon-yinle1'></i> 左下角可以打开博客歌单，不知道有没有合你胃口的音乐呢？</br > <i class='iconfont icon-shezhi1'></i> 右下角点击 <i class= 'fas fa-cog fa-spin'></i > 可以展开更多设置。</div>",
         // html: "<i class='iconfont icon-lihua'></i> 欢迎来到 Juruoer 的博客！</br> <i class='iconfont icon-yinle1'></i> 左下角可以打开音乐播放器，不知道有没有合你胃口的音乐呢？</br > <i class='iconfont icon-shezhi1'></i> 右下角点击 <i class= 'fas fa-cog fa-spin'></i > 可以展开更多设置。",
         allowOutsideClick: false,
         width: '25rem'
